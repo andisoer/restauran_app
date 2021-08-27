8d3fe723ec1dee597d6900a9eb67b4682a241ae8
Andi Surya <andi502.as@gmail.com>
2021-08-27 17:02:04 +0700

    (WIP) Search

--- lib/page/search.dart
+++ lib/page/search.dart
@@ -17,68 +17,81 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  List<Restaurant> restaurants = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-          children: [
-            Container(
-              color: primaryColor,
-              child: SafeArea(
-                child: Container(
-                  margin: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(Radius.circular(8)),
-                      boxShadow: [
-                        BoxShadow(
-                            color: greyColor.withAlpha(70),
-                            spreadRadius: 1,
-                            blurRadius: 10)
-                      ]),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                        hintText: 'Search restaurant...',
-                        labelText: null,
-                        enabledBorder: searchTextFieldFormStyle(),
-                        border: searchTextFieldFormStyle(),
-                        focusedBorder: searchTextFieldFormStyle(),
-                        prefixIcon: Padding(
-                          padding: EdgeInsets.only(left: 8),
-                          child: IconButton(
-                            icon: Icon(Icons.arrow_back_ios, color: primaryColor,),
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
+        children: [
+          Container(
+            color: primaryColor,
+            child: SafeArea(
+              child: Container(
+                margin: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                    boxShadow: [
+                      BoxShadow(
+                          color: greyColor.withAlpha(70),
+                          spreadRadius: 1,
+                          blurRadius: 10)
+                    ]),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      hintText: 'Search restaurant...',
+                      labelText: null,
+                      enabledBorder: searchTextFieldFormStyle(),
+                      border: searchTextFieldFormStyle(),
+                      focusedBorder: searchTextFieldFormStyle(),
+                      prefixIcon: Padding(
+                        padding: EdgeInsets.only(left: 8),
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.arrow_back_ios,
+                            color: primaryColor,
                           ),
-                        )),
-                  ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      )),
                 ),
               ),
             ),
-            Expanded(
-              child: Container(
+          ),
+          Expanded(
+            child: Container(
                 margin: EdgeInsets.symmetric(horizontal: 16),
-                child: FutureBuilder<String>(
-                    future: DefaultAssetBundle.of(context)
-                        .loadString('assets/restaurants.json'),
-                    builder: (context, snapshot) {
-                      final restaurants =
-                          parseRestaurantsFromJson(snapshot.data);
-                      return ListView.builder(
-                        padding: EdgeInsets.symmetric(vertical: 8),
-                        shrinkWrap: true,
-                        itemBuilder: (context, index) {
-                          return buildRestaurantItem(
-                              context, restaurants[index]);
-                        },
-                        itemCount: restaurants.length,
-                      );
-                    }),
-              ),
-            )
-          ],
+                child: restaurants.length > 0
+                    ? ListView.builder(
+                  padding: EdgeInsets.symmetric(vertical: 8),
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return buildRestaurantItem(
+                        context, restaurants[index]);
+                  },
+                  itemCount: restaurants.length,
+                )
+                    : Center(
+                  child: Text('Ooops, no restaurant found !'),
+                )),
+          )
+        ],
       ),
     );
+  }
+
+  // void loadRestaurants() {
+  //   var stringAsset = DefaultAssetBundle.of(context)
+  //       .loadString('assets/restaurants.json');
+  //
+  //   parseRestaurantsFromJson(stringAsset);
+  // }
+
+  @override
+  void initState() {
+    super.initState();
   }
 }
