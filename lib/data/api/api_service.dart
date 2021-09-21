3285da76e7b08ff6371ee680b1d200e89163d71a
Andi Surya <andi502.as@gmail.com>
2021-09-21 11:02:24 +0700

    Add api_service.dart and restaurant.dart

---
+++ lib/data/api/api_service.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import 'package:restauran_app/data/model/restaurant.dart';
+
+class ApiService {
+  static final String _baseUrl = "https://restaurant-api.dicoding.dev/";
+
+  Future<RestaurantResult> listRestaurant() async {
+    final response = await http.get(Uri.parse(_baseUrl + 'list'));
+
+    if (response.statusCode == 200) {
+      return RestaurantResult.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load restaurants');
+    }
+  }
+
+  Future<DetailRestaurantResult> detailRestaurant(String restaurantId) async {
+    final response =
+        await http.get(Uri.parse(_baseUrl + 'detail/' + restaurantId));
+
+    if (response.statusCode == 200) {
+      return DetailRestaurantResult.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load detail restaurant');
+    }
+  }
+}
