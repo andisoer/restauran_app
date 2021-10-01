6f45bdf27c441647232de20013652120fd7de5ce
Andi Surya <andi502.as@gmail.com>
2021-10-01 13:35:28 +0700

    Test parsing restaurant detail json

--- test/test_api.dart
+++ test/test_api.dart
@@ -1,35 +1,32 @@
 import 'dart:convert';
 import 'dart:io';
-import 'dart:math';
-
 import 'package:flutter_test/flutter_test.dart';
+import 'package:restauran_app/data/model/detail_restaurant.dart';
 import 'package:restauran_app/data/model/list_restaurant.dart';
 
 void main() {
-  group('Parsing Testing', () {
-    test('Test Parsing JSON 1', () {
-      final testFileRestaurant = File('test/test_resource/restaurant.json').readAsStringSync();
-      final restaurant = RestaurantResult.fromJson(jsonDecode(testFileRestaurant) as Map<String, dynamic>);
+  group('Parsing JSON', () {
+    test('Parsing Restaurant JSON List', () async {
+      final testFileRestaurant =
+          File('test/test_resource/restaurant.json').readAsStringSync();
+      final restaurant = RestaurantResult.fromJson(
+          jsonDecode(testFileRestaurant) as Map<String, dynamic>);
 
       expect(restaurant.error, false);
       expect(restaurant.message, "success");
       expect(restaurant.restaurants.length, 1);
+      expect(restaurant.restaurants[0].name, "Melting Pot");
+    });
 
-      // final restaurant = Restaurant(
-      //   id: '1',
-      //   name: 'Warung',
-      //   description: 'Deskripsi',
-      //   pictureId: 'PictureId',
-      //   city: 'City',
-      //   rating: 0,
-      // );
-      //
-      // expect(restaurant.id, '1');
-      // expect(restaurant.name, 'Warung');
-      // expect(restaurant.description, 'Deskripsi');
-      // expect(restaurant.pictureId, 'PictureId');
-      // expect(restaurant.city, 'City');
-      // expect(restaurant.rating, 0);
+    test('Parsing Restaurant JSON Detail', () async {
+      final testFileRestaurant =
+          File('test/test_resource/restaurant_detail.json').readAsStringSync();
+      final restaurant = DetailRestaurantResult.fromJson(
+          jsonDecode(testFileRestaurant) as Map<String, dynamic>);
+
+      expect(restaurant.error, false);
+      expect(restaurant.message, "success");
+      expect(restaurant.restaurant.name, "Melting Pot");
     });
   });
 }
